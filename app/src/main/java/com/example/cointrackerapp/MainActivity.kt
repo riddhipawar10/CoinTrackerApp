181ce2e223c0fb85145eb13662a1238f23f67d00
riddhipawar10 <rspawar1004@gmail.com>
2026-03-26 23:57:04 +0530

    Fixed API integration using CoinGecko and add calculateTopPadding in MainActivity.kt

--- app/src/main/java/com/example/cointrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/cointrackerapp/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.example.cointrackerapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.cointrackerapp.viewmodel.CoinViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.cointrackerapp.presentation.screen.CoinListScreen
-import kotlinx.coroutines.flow.asStateFlow
 import com.example.cointrackerapp.ui.theme.CoinTrackerAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
@@ -28,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent{
             CoinTrackerAppTheme {
                 Scaffold {
-                    CoinListScreen(modifier = Modifier.padding(it))
+                    CoinListScreen(modifier = Modifier.padding(it.calculateTopPadding()))
                 }
             }
         }
--- app/src/main/java/com/example/cointrackerapp/remote/api/CoinAPI.kt
+++ app/src/main/java/com/example/cointrackerapp/remote/api/CoinAPI.kt
@@ -5,6 +5,6 @@ import com.example.cointrackerapp.remote.model.response.CoinGecko
 import retrofit2.http.GET
 
 interface CoinAPI {
-    @GET("coins/markets?vs_currency=usd&per_page=10")
+    @GET("coins/markets?vs_currency=usd")
     suspend fun getCoinList(): List<CoinGecko>
 }
